5663afbd69fe6ef44a5036d83f8c579416dd125f
Shaheer-codex <shaheergaming766@gmail.com>
2026-05-20 16:58:02 +0500

    Add files via upload

---
+++ A1.py
@@ -0,0 +1,15 @@
+tuplex = ("tuple", False , 3.2, 1)
+print(tuplex)
+tuplex = (6, 5, 3, 5 ,7 ,2 ,9)
+print(tuplex)
+tuplex = tuplex + (9,)
+print(tuplex)
+
+tuple1 = (20, 50 , 30, 20, 50)
+print(tuple1.count(50))
+
+tuplex = (2,3,8,5,4,6,3,8,9,7)
+_slice = tuplex[3:5]
+print(_slice)
+_slice = tuplex[:6]
+print(_slice)
